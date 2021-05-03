ca277898fb0f6a939a30de9c43ccac8e43336d78
realm-defi <bsc.realm.finance@gmail.com>
2021-05-03 20:51:31 +0200

    chore: linting and naming

--- src/views/Boardroom/components/DepositModal.tsx
+++ src/views/Boardroom/components/DepositModal.tsx
@@ -1,52 +1,60 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react';
+import styled from 'styled-components';
 
-import Button from '../../../components/Button'
-import Modal, { ModalProps } from '../../../components/Modal'
-import ModalActions from '../../../components/ModalActions'
-import ModalTitle from '../../../components/ModalTitle'
-import TokenInput from '../../../components/TokenInput'
+import Button from '../../../components/Button';
+import Modal, { ModalProps } from '../../../components/Modal';
+import ModalActions from '../../../components/ModalActions';
+import ModalTitle from '../../../components/ModalTitle';
+import TokenInput from '../../../components/TokenInput';
 
-import { getFullDisplayBalance } from '../../../utils/formatBalance'
+import { getFullDisplayBalance } from '../../../utils/formatBalance';
 import { BigNumber } from 'ethers';
 
 interface DepositModalProps extends ModalProps {
-  max: BigNumber,
-  onConfirm: (amount: string) => void,
-  tokenName?: string,
+  max: BigNumber;
+  onConfirm: (amount: string) => void;
+  tokenName?: string;
 }
 
-const DepositModal: React.FC<DepositModalProps> = ({ max, onConfirm, onDismiss, tokenName = '' }) => {
-  const [val, setVal] = useState('')
+const DepositModal: React.FC<DepositModalProps> = ({
+  max,
+  onConfirm,
+  onDismiss,
+  tokenName = '',
+}) => {
+  const [val, setVal] = useState('');
 
   const fullBalance = useMemo(() => {
-    return getFullDisplayBalance(max, tokenName === 'USDC' ? 6 : 18)
-  }, [max])
+    return getFullDisplayBalance(max, tokenName === 'USDC' ? 6 : 18);
+  }, [max, tokenName]);
 
-  const handleChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    setVal(e.currentTarget.value)
-  }, [setVal])
+  const handleChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setVal(e.currentTarget.value);
+    },
+    [setVal],
+  );
 
   const handleSelectMax = useCallback(() => {
-    setVal(fullBalance)
-  }, [fullBalance, setVal])
+    setVal(fullBalance);
+  }, [fullBalance, setVal]);
 
+  console.log(fullBalance);
   return (
     <Modal>
       <ModalTitle text={`Deposit ${tokenName}`} />
-      <TokenInput
-        value={val}
-        onSelectMax={handleSelectMax}
-        onChange={handleChange}
-        max={fullBalance}
-        symbol={tokenName}
-      />
+      <Label>Available Balance: {fullBalance}</Label>
+      <TokenInput value={val} onSelectMax={handleSelectMax} onChange={handleChange} />
       <ModalActions>
-        <Button text="Cancel" variant="secondary" onClick={onDismiss} />
-        <Button text="Confirm" onClick={() => onConfirm(val)} />
+        <Button text="Cancel" variant="exiled" onClick={onDismiss} />
+        <Button text="Confirm" variant="peons" onClick={() => onConfirm(val)} />
       </ModalActions>
     </Modal>
-  )
-}
+  );
+};
 
+const Label = styled.span`
+  color: ${({ theme }) => theme.color.primary.main};
+`;
 
-export default DepositModal
+export default DepositModal;
--- src/views/Boardroom/components/WithdrawModal.tsx
+++ src/views/Boardroom/components/WithdrawModal.tsx
@@ -1,51 +1,59 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react';
+import styled from 'styled-components';
 
-import Button from '../../../components/Button'
-import Modal, { ModalProps } from '../../../components/Modal'
-import ModalActions from '../../../components/ModalActions'
-import ModalTitle from '../../../components/ModalTitle'
-import TokenInput from '../../../components/TokenInput'
+import Button from '../../../components/Button';
+import Modal, { ModalProps } from '../../../components/Modal';
+import ModalActions from '../../../components/ModalActions';
+import ModalTitle from '../../../components/ModalTitle';
+import TokenInput from '../../../components/TokenInput';
 
-import { getFullDisplayBalance } from '../../../utils/formatBalance'
+import { getFullDisplayBalance } from '../../../utils/formatBalance';
 import { BigNumber } from 'ethers';
 
 interface WithdrawModalProps extends ModalProps {
-  max: BigNumber,
-  onConfirm: (amount: string) => void,
-  tokenName?: string,
+  max: BigNumber;
+  onConfirm: (amount: string) => void;
+  tokenName?: string;
 }
 
-const WithdrawModal: React.FC<WithdrawModalProps> = ({ onConfirm, onDismiss, max, tokenName = '' }) => {
-  const [val, setVal] = useState('')
+const WithdrawModal: React.FC<WithdrawModalProps> = ({
+  onConfirm,
+  onDismiss,
+  max,
+  tokenName = '',
+}) => {
+  const [val, setVal] = useState('');
 
   const fullBalance = useMemo(() => {
-    return getFullDisplayBalance(max)
-  }, [max])
+    return getFullDisplayBalance(max);
+  }, [max]);
 
-  const handleChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    setVal(e.currentTarget.value)
-  }, [setVal])
+  const handleChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setVal(e.currentTarget.value);
+    },
+    [setVal],
+  );
 
   const handleSelectMax = useCallback(() => {
-    setVal(fullBalance)
-  }, [fullBalance, setVal])
+    setVal(fullBalance);
+  }, [fullBalance, setVal]);
 
   return (
     <Modal>
       <ModalTitle text={`Withdraw ${tokenName}`} />
-      <TokenInput
-        onSelectMax={handleSelectMax}
-        onChange={handleChange}
-        value={val}
-        max={fullBalance}
-        symbol={tokenName}
-      />
+      <Label>Available Balance: {fullBalance}</Label>
+      <TokenInput onSelectMax={handleSelectMax} onChange={handleChange} value={val} />
       <ModalActions>
-        <Button text="Cancel" variant="secondary" onClick={onDismiss} />
-        <Button text="Confirm" onClick={() => onConfirm(val)} />
+        <Button text="Cancel" variant="exiled" onClick={onDismiss} />
+        <Button text="Confirm" variant="peons" onClick={() => onConfirm(val)} />
       </ModalActions>
     </Modal>
-  )
-}
+  );
+};
 
-export default WithdrawModal
+const Label = styled.span`
+  color: ${({ theme }) => theme.color.primary.main};
+`;
+
+export default WithdrawModal;
